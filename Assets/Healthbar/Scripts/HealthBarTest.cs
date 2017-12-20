e1fa10ed4a3f0814fe30b57380871f05a3834812
Manindu Wijewickrama <manindu90@gmail.com>
2017-12-21 00:30:39 +0530

    first commit

---
+++ Assets/Healthbar/Scripts/HealthBarTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBarTest : MonoBehaviour
+{
+    [SerializeField] float deduction = 10f;
+    [SerializeField] float increment = 10f;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Healthbar.instance.ReduceValue(deduction);
+        }
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Healthbar.instance.IncreaseValue(increment);
+        }
+    }
+}
---
+++ Assets/Healthbar/Scripts/Healthbar.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour
+{
+
+    public static Healthbar instance;
+
+    [SerializeField] Image barFill;
+    [SerializeField] float transitionDuration = 0.5f;
+
+    float health = 100f;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public void ReduceValue(float deduction)
+    {
+        float targetValue = Mathf.Max(0, (health - deduction));
+        StartCoroutine(LerpValue(health, targetValue, transitionDuration));
+    }
+
+    public void IncreaseValue(float increment)
+    {
+        float targetValue = Mathf.Min(100, (health + increment));
+        StartCoroutine(LerpValue(health, targetValue, transitionDuration));
+    }
+
+    IEnumerator LerpValue(float currentValue, float targetValue, float duration)
+    {
+        float elapsed = 0f;
+        while(elapsed < duration)
+        {
+            health = Mathf.Lerp(currentValue, targetValue, (elapsed / duration));
+            barFill.fillAmount = health / 100f;
+            elapsed += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        health = targetValue;
+    }
+}
